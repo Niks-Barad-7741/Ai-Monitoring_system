ad15f60c4bd1970ddc3fd2dc047ed9031e1897d5
Nikhil Barad <nikhilbarad465@gmail.com>
2026-04-01 15:58:45 +0530

    Added Proxy settings for  the deploymet in IIS works on both(Local and Hosted) with this

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': 'http://127.0.0.1:8000',
+      '/dashboard': 'http://127.0.0.1:8000',
+      '/ai': 'http://127.0.0.1:8000',
+      '/admin/admin-logs': 'http://127.0.0.1:8000',
+      '/admin/admin-analytics': 'http://127.0.0.1:8000',
+      '/user/user-logs': 'http://127.0.0.1:8000',
+      '/user/user-analytics': 'http://127.0.0.1:8000',
+      '/ws': {
+        target: 'ws://127.0.0.1:8000',
+        ws: true,
+      },
+    }
+  }
 })
